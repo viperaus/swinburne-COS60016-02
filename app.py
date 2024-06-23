74bb03ebc4d1e2039fb386929131fbd86c443353
Troy <viperaus@users.noreply.github.com>
2024-06-23 18:21:43 +1000

    Add comments

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import logging
 logging.basicConfig(level=logging.INFO)
 custom_logger = logging.getLogger(__name__)
 
+# check if training has been completed
 training_data_exists = os.path.exists(".training_completed")
 if not training_data_exists:
     print("Training data not found. Please run the training script first using the command 'make setup'")
     exit()
 
-app = Flask(__name__)
-
+# initialize chatbot
 chatbot = ChatBot('Ronnie',
                   storage_adapter='chatterbot.storage.SQLStorageAdapter', 
                   preprocessors=['chatterbot.preprocessors.clean_whitespace','chatterbot.preprocessors.unescape_html','chatterbot.preprocessors.convert_to_ascii'],
@@ -22,14 +22,20 @@ chatbot = ChatBot('Ronnie',
                   read_only=True,
                   logger=custom_logger)
 
+# add logic adapters
 chatbot.logic_adapters.append(WeatherInCityAdapter(chatbot))
 chatbot.logic_adapters.append(BestMatch(chatbot, excluded_words=["time"]))
 chatbot.logic_adapters.append(TimeLogicAdapter(chatbot))
 
+# initialize flask app
+app = Flask(__name__)
+
+# add route for home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# add route for chatbot response
 @app.route("/get")
 def get_bot_response():
     user_input = request.args.get('question')
